a54126f88583e58aa5b4b3a9a7302a6a2274bd4a
ihsannrs21-lab <ihsannrs21@gmail.com>
2026-05-09 14:32:32 +0700

    Fix download script to use correct Gradle Services URL

--- download-jar.bat
+++ download-jar.bat
@@ -6,23 +6,23 @@ echo Gradle Wrapper JAR Download
 echo ================================================
 echo.
 
+REM Download gradle-wrapper.jar directly from Gradle Services
 echo Downloading gradle-wrapper.jar from Gradle Services...
 echo.
 
 if not exist "gradle\wrapper" mkdir gradle\wrapper
 
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri 'https://services.gradle.org/wrapper/gradle-wrapper-8.4.jar' -OutFile 'gradle\wrapper\gradle-wrapper.jar'"
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://services.gradle.org/wrapper/gradle-wrapper-8.4.jar' -OutFile 'gradle\wrapper\gradle-wrapper.jar' -Verbose"
 
 if exist "gradle\wrapper\gradle-wrapper.jar" (
-    for %%A in ("gradle\wrapper\gradle-wrapper.jar") do (
-        set SIZE=%%~zA
-    )
     echo.
-    echo SUCCESS! gradle-wrapper.jar downloaded ^(!SIZE! bytes^)
+    echo SUCCESS! gradle-wrapper.jar has been downloaded.
     echo You can now run: gradlew.bat run
 ) else (
     echo.
     echo ERROR: Failed to download gradle-wrapper.jar
+    echo Please try again or download manually from:
+    echo https://services.gradle.org/wrapper/gradle-wrapper-8.4.jar
 )
 
 echo.
